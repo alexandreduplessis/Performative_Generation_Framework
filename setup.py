9c8961629d2b4df5d8ff1c00b0d3bfc356449b87
alexandreduplessis <alexandre.duplessis@ens.psl.eu>
2023-06-16 15:08:53 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='perfgen',
       install_requires=[
-            "numpy","torch", "ot", "sklearn", "tqdm", 'scipy>=0.18.0', 'matplotlib>=2.0.0',
-            'scikit-learn>=1.0', 'pandas', 'ipython'],
+            "numpy","torch", "pot", "tqdm", 'scipy>=0.18.0', 'matplotlib>=2.0.0',
+            'scikit-learn>=1.0', 'ipython'],
       packages=['perfgen'],
       )
