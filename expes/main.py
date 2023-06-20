d53946e843e6aa5cd4a5db9152f3a744e81f7d82
alexandreduplessis <alexandre.duplessis@ens.psl.eu>
2023-06-20 15:35:43 -0400

    Move Generator Class

--- expes/main.py
+++ expes/main.py
@@ -3,84 +3,12 @@ import torch
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import argparse
-from perfgen.utils import mix_data
 from perfgen.models.gaussian1D import Gaussian_Estimator_1D
 from perfgen.models.gmm1D import Gaussian_Mixture_Model_1D
 from perfgen.models.gmm import Gaussian_Mixture_Model
 from datasets import two_moons_dataset
+from perfgen.generator import Performative_Generator
 
-class Performative_Generator():
-    """
-    Performative Generator
-
-    Parameters
-    ----------
-    model : object
-        Model to train and generate data
-        model is assumed to have
-            - a train method of the form model.train(data, epochs, **kwargs)
-            - a generate method of the form model.generate(nb_samples, **kwargs)
-            - an eval method of the form model.eval(data, **kwargs) that returns a dictionary of metrics
-    data : array
-        Old data
-    nb_iters : int
-        Number of iterations
-    prop_old_schedule : array
-        Proportion of old data at each iteration
-    nb_new_schedule : array
-        Number of new datapoints to generate at each iteration
-    epochs_schedule : array
-        Number of epochs to train the model at each iteration
-    eval_schedule : array
-        Iterations at which to evaluate the model
-    eval_datas : array
-        Dataset on which to evaluate the model
-    """
-    def __init__(self, model, data, nb_iters, prop_old_schedule, nb_new_schedule, epochs_schedule, eval_schedule, eval_data=None):
-        self.model = model
-        self.data = data
-        self.old_data = data.copy()
-        self.nb_iters = nb_iters
-        self.prop_old_schedule = prop_old_schedule
-        self.nb_new_schedule = nb_new_schedule
-        self.epochs_schedule = epochs_schedule
-        self.eval_schedule = eval_schedule
-        self.eval_data = eval_data
-
-
-    def train(self):
-        metrics = {}
-        metrics['indices'] = self.eval_schedule
-        for i in tqdm(range(self.nb_iters)):
-            # Generate data to train
-            nb_old = int(self.prop_old_schedule[i] * self.data.shape[0])
-            nb_new = self.nb_new_schedule[i]
-            data_to_train = mix_data(self.old_data[:nb_old], self.model.generate(nb_new).reshape((nb_new, self.old_data.shape[1])))
-            self.data = data_to_train.copy() # not useful
-
-            # Train model
-            losses = self.model.train(data_to_train, self.epochs_schedule[i])
-
-            if i in self.eval_schedule:
-                # Evaluate on old_data
-                new_metrics = self.model.eval(self.old_data)
-                if i == self.eval_schedule[0]:
-                    for keys in new_metrics.keys():
-                        metrics["old"+str(keys)] = np.array([new_metrics[keys]])
-                else:
-                    for keys in new_metrics.keys():
-                        metrics["old"+str(keys)] = np.concatenate([metrics["old"+str(keys)], np.array([new_metrics[keys]])])
-                if self.eval_data is not None:
-                    # Evaluate on eval_data
-                    new_metrics = self.model.eval(self.old_data)
-                    if i == self.eval_schedule[0]:
-                        for keys in new_metrics.keys():
-                            metrics["eval"+str(keys)] = np.array([new_metrics[keys]])
-                    else:
-                        for keys in new_metrics.keys():
-                            metrics["eval"+str(keys)] = np.concatenate([metrics["eval"+keys], np.array([new_metrics[keys]])])
-
-        return metrics
 
 
 if __name__ == "__main__":
