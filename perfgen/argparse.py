88afb7715c70ddfa27c484fbf575690cb1216201
QB3 <quentin.bertrand@inria.fr>
2023-08-24 11:33:47 -0400

    investigate optimizer

--- perfgen/argparse.py
+++ perfgen/argparse.py
@@ -4,7 +4,8 @@ import torch
 from omegaconf import OmegaConf
 
 
-def my_parser(path_to_yaml='configs/cifar.yaml'):
+def my_parser(path_to_yaml='configs/investigate_bug_optimizer.yaml'):
+    # def my_parser(path_to_yaml='configs/cifar.yaml'):
     # def my_parser(path_to_yaml='configs/2D_toy_collapse.yaml'):
     yaml_config = OmegaConf.load(path_to_yaml)
     parser = generate_parser_from_dict(yaml_config)
